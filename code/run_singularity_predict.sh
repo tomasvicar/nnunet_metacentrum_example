3528e6404f695d2874615b6ab89179158b298be4
Jiri Chmelik <58982782+ChmelJ@users.noreply.github.com>
2023-08-22 13:12:44 +0200

    new scripts for nnUNetv2

--- code/run_singularity_predict.sh
+++ code/run_singularity_predict.sh
@@ -13,19 +13,20 @@ cd $CODEDIR
 # install aditional packages (you can add custom packages with pip)
 export PYTHONUSERBASE=$SCRATCHDIR
 export PATH=$PYTHONUSERBASE/bin:$PATH
-export PYTHONPATH=$PYTHONUSERBASE/lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPATH=$PYTHONUSERBASE/lib/python3.10/site-packages:$PYTHONPATH
 pip install -e $CODEDIR/nnUNet
 
 # run python script with RESULTSDIR as input argument (which is then avalible in python)
 
 # set nnUNet paths
-export nnUNet_raw_data_base=$DATADIR
+export nnUNet_raw=$DATADIR
 export nnUNet_preprocessed=$PREPROCESSDIR
-export RESULTS_FOLDER=$RESULTSDIR
+export nnUNet_results=$RESULTSDIR
 
-# run nnUNet training
-# -m 2d or 3d_fullres
-# -t TASK ID (502)
-# -i input folder path ($DATADIR/Task502_ALD_Singularity_test/imagesTs)
+# run nnUNet predict
+# -i input folder path ($DATADIR/nnUNet_raw/Task502_ALD_Singularity_test/imagesTs)
 # -o output folder path ($RESULTSDIR/predict/Task502_ALD_Singularity_test)
-nnUNet_predict -i $DATADIR/nnUNet_raw_data/Task502_ALD_Singularity_test/imagesTs -o $RESULTSDIR/predict/Task502_ALD_Singularity_test -t 502 -m 3d_fullres
+# -c 2d or 3d_fullres
+# -d Dataset ID (001)
+# -f exact fold (0,1,2,3,4, all)
+nnUNetv2_predict -i $DATADIR/nnUNet_raw/Dataset001_GrayMatter/imagesTs -o $RESULTSDIR/predict/Dataset001_GrayMatter/fold_4 -d 001 -c 2d -f 4
