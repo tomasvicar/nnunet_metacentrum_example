309e7f9478eb969ce9001ac1f98a8a95c995dd65
Jiri Chmelik <58982782+ChmelJ@users.noreply.github.com>
2023-08-22 13:14:06 +0200

    new scripts for nnUNet v2

--- code/run_singularity_plan.sh
+++ code/run_singularity_plan.sh
@@ -13,16 +13,16 @@ cd $CODEDIR
 # install aditional packages (you can add custom packages with pip)
 export PYTHONUSERBASE=$SCRATCHDIR
 export PATH=$PYTHONUSERBASE/bin:$PATH
-export PYTHONPATH=$PYTHONUSERBASE/lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPATH=$PYTHONUSERBASE/lib/python3.10/site-packages:$PYTHONPATH
 pip install -e $CODEDIR/nnUNet
 
 # run python script with RESULTSDIR as input argument (which is then avalible in python)
 
 # set nnUNet paths
-export nnUNet_raw_data_base=$DATADIR
+export nnUNet_raw=$DATADIR
 export nnUNet_preprocessed=$PREPROCESSDIR
-export RESULTS_FOLDER=$RESULTSDIR
+export nnUNet_results=$RESULTSDIR
 
-# run planning and preprocessing -t TASK ID (502)
-nnUNet_plan_and_preprocess -t 502 --verify_dataset_integrity
+# run planning and preprocessing -d TASK ID (502)
+nnUNetv2_plan_and_preprocess -d 001 --verify_dataset_integrity
 cp -R $PREPROCESSDIR $PREPROCESSDIR_ORIG/..
--- code/run_singularity_train.sh
+++ code/run_singularity_train.sh
@@ -13,19 +13,19 @@ cd $CODEDIR
 # install aditional packages (you can add custom packages with pip)
 export PYTHONUSERBASE=$SCRATCHDIR
 export PATH=$PYTHONUSERBASE/bin:$PATH
-export PYTHONPATH=$PYTHONUSERBASE/lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPATH=$PYTHONUSERBASE/lib/python3.10/site-packages:$PYTHONPATH
 pip install -e $CODEDIR/nnUNet
 
 # run python script with RESULTSDIR as input argument (which is then avalible in python)
 
 # set nnUNet paths
-export nnUNet_raw_data_base=$DATADIR
+export nnUNet_raw=$DATADIR
 export nnUNet_preprocessed=$PREPROCESSDIR
-export RESULTS_FOLDER=$RESULTSDIR
+export nnUNet_results=$RESULTSDIR
 
 # run nnUNet training
+# Dataset ID (001)
 # 2d or 3d_fullres
-# TASK ID (502)
-# FOLD number 0, 1, 2, 3, 4, 5, 'all'
+# FOLD number 0, 1, 2, 3, 4, 5, all
 # -c continue training from last epoch
-nnUNet_train 3d_fullres nnUNetTrainerV2 502 0
+nnUNetv2_train 001 2d 0
